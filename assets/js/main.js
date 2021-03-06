92cd1a4b394a9515e3e44ff8afa40e7dd2aa4670
Charles Perry <76476005+Ctperry1@users.noreply.github.com>
2021-03-06 11:00:59 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    //sticky navbar
     window.addEventListener("scroll", function (e) {
         if (window.pageYOffset > 50) {
             document.querySelector("header").classList.add("is-scrolling");
@@ -6,28 +7,27 @@ window.onload = function () {
             document.querySelector("header").classList.remove("is-scrolling");
         }
     });
-
+    //get mobile nav menu
     const menu_btn = document.querySelector(".hamburger");
     const mobile_menu = document.querySelector(".mobile-nav");
-
+    //toggle mobile nav menu
     menu_btn.addEventListener("click", function () {
         menu_btn.classList.toggle("is-active");
         mobile_menu.classList.toggle("is-active");
     });
-
+    //toggle mobile nav menu
     mobile_menu.addEventListener("click", function () {
         menu_btn.classList.toggle("is-active");
         mobile_menu.classList.toggle("is-active");
     });
 
     //Get the button:
-    mybutton = document.getElementById("myBtn");
+    mybutton = document.getElementById("back2Top");
 
-    // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function () {
         scrollFunction();
     };
-
+    //show button on scroll
     function scrollFunction() {
         if (
             document.body.scrollTop > 20 ||
@@ -38,8 +38,7 @@ window.onload = function () {
             mybutton.style.display = "none";
         }
     }
-
-    // When the user clicks on the button, scroll to the top of the document
+    //back to top of page on click
     function topFunction() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
